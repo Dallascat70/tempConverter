bf046a1a9d9e14c0404c4c404b98afa29930da0f
Dallascat70 <alyssa.prah123@west-mec.org>
2016-10-20 15:19:44 -0700

    Add files via upload

---
+++ TemperatureConverter.js
@@ -0,0 +1,24 @@
+
+
+function convert(degree) {
+    var deg;
+    if (degree == "C") {
+        deg = document.getElementById("c").value * 9 / 5 + 32;
+        document.getElementById("f").value = Math.round(deg);
+
+    }
+
+    else {
+
+        deg = (document.getElementById("f").value - 32) * 5 / 9;
+        document.getElementById("c").value = Math.round(deg);
+    }
+
+    if (isNaN(deg.value)) { 
+        deg.value = "Enter a valid number";
+}}
+
+
+
+
+
